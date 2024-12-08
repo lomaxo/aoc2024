0f5c2e19d2542056beb33c6b770e6c81e7b333f4
Oliver Lomax <oliver.lomax@abingdon.org.uk>
2024-12-08 19:38:25 +0000

    Day 6 part 2.

--- day6.py
+++ day6.py
@@ -1,3 +1,5 @@
+from copy import deepcopy
+
 data = '''....#.....
 .........#
 ..........
@@ -25,6 +27,17 @@ y_dir = -1
 def rotate(x, y):
     return -y, x
 
+def get_dir_marker(x, y):
+    if x > 0:
+        return '>'
+    elif x < 0:
+        return '<'
+    elif y < 0:
+        return '^'
+    else:
+        return 'v'
+
+
 # Find "^"
 start_x = 0
 start_y = 0
@@ -34,6 +47,8 @@ for y in range(y_size):
             start_x, start_y = x, y
             
 print(start_x, start_y)
+
+# Part 1
 x, y = start_x, start_y
 while x >= 0 and x < x_size and y >=0 and y < y_size:
     if mapdata[y][x] == '#':
@@ -47,7 +62,37 @@ while x >= 0 and x < x_size and y >=0 and y < y_size:
 
 count = 0
 for row in mapdata:
-    print(row)
+    # print(row)
     count += row.count('X')
 
 print(f"Total spaces = {count}")
+
+# Part 2
+def is_loop(mapdata, x, y):
+    x_dir, y_dir = 0, -1
+    been_here = set()
+    while x >= 0 and x < x_size and y >=0 and y < y_size:
+        if mapdata[y][x] == '#':
+            x -= x_dir
+            y -= y_dir
+            x_dir, y_dir = rotate(x_dir, y_dir)
+        else:
+            if (x, y, x_dir, y_dir) in been_here:
+                return True
+            mapdata[y][x] = get_dir_marker(x_dir, y_dir)
+            been_here.add((x, y, x_dir, y_dir))
+        x += x_dir
+        y += y_dir
+
+    return False
+
+total = 0
+for y in range(len(mapdata)):
+    for x in range(len(mapdata[0])):
+        map_copy = deepcopy(mapdata)
+        map_copy[y][x] = '#'
+        # print(f"testing x={x}, y={y}")
+        if is_loop(map_copy, start_x, start_y):
+            total += 1
+
+print(total)
